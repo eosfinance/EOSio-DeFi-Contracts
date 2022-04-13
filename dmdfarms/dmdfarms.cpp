a92542513195c5ea2d0c607e918c1fb42a818c15
Glue Dog <32752940+gluedog@users.noreply.github.com>
2022-04-13 21:04:10 +0700

    Update dmdfarms.cpp

--- dmdfarms/dmdfarms.cpp
+++ dmdfarms/dmdfarms.cpp
@@ -13,7 +13,7 @@ DMD Yeld farms.
 
 */
 
-void dmdfarms::setpool(uint16_t pool_id, uint32_t dmd_issue_frequency, bool locked, uint64_t min_lp_tokens, const& asset asset_symbol, const& string pool_name)
+void dmdfarms::setpool(uint16_t pool_id, uint32_t dmd_issue_frequency, bool is_active, uint64_t min_lp_tokens, const& asset box_asset_symbol, const& string pool_name, uint64_t dmd_mine_qty_remaining)
 {
     require_auth(get_self());
 
@@ -25,12 +25,12 @@ void dmdfarms::setpool(uint16_t pool_id, uint32_t dmd_issue_frequency, bool lock
     uint32_t months_between_halvings = 3;
 
     if(total_it == pool_stats.end()) 
-    { // Some of these rows will only get modified the first time set() is ran:
+    { /* Some of these rows will only get modified the first time setpool() is ran */
         pool_stats.emplace(get_self(), [&](auto& row) 
         {
             row.key = pool_id;
             
-            row.locked = locked;
+            row.is_active = is_active;
             /* We can keep the halvings, and these would be applied separately, to each pool. Not a bad thing to keep. */
             row.mining_start_time = now;
             row.halving1_deadline = now+(1*seconds_in_a_month * months_between_halvings);
@@ -39,70 +39,62 @@ void dmdfarms::setpool(uint16_t pool_id, uint32_t dmd_issue_frequency, bool lock
             row.halving4_deadline = now+(4*seconds_in_a_month * months_between_halvings);
             row.last_reward_time = now;
 
+            row.dmd_mine_qty_remaining = dmd_mine_qty_remaining; /* How many DMDs are left to be mined in the pool */
             row.dmd_issue_frequency = dmd_issue_frequency;
-            row.minimum_lp_tokens = min_lp_tokens; /* Minimum LP tokens required to earn yield in the pool. Needs to check for this on issue(). */
-            row.asset_symbol = asset_symbol;       /* The BOX-LP Tokens used to identify the pair for the pool. */
-            row.pool_name = pool_name;             /* String identifying the pool name, for display purposes only. */
+            row.minimum_lp_tokens = min_lp_tokens;   /* Minimum LP tokens required to earn yield in the pool. Needs to check for this on issue(). */
+            row.box_asset_symbol = box_asset_symbol; /* The BOX-LP Tokens used to identify the pair for the pool. */
+            row.pool_name = pool_name;               /* String identifying the pool name, for display purposes only. */
         });
     }
     else
         pool_stats.modify(total_it, get_self(),[&]( auto& row) 
         { // We can always modify these rows with set() at a later date:
-            row.locked = locked;
+            row.is_active = is_active;
 
             row.dmd_issue_frequency = dmd_issue_frequency;
+            row.dmd_mine_qty_remaining = dmd_mine_qty_remaining; /* How many DMDs are left to be mined in the pool */
             row.minimum_lp_tokens = min_lp_tokens;
             row.asset_symbol = asset_symbol;
+            row.pool_name = pool_name;
 
             row.last_reward_time = now;
         });
 }
 
-void efimine::issue(uint16_t pool_id)
-{   /* The worker will do an issue for every pool, to help with the batching */
+void efimine::issue(uint16_t pool_id) /* Should add an offset here to control batching */
+{   /* The worker will do an issue for each individual pool. */
     require_auth( "worker.efi"_n );
 
     /* Mining rate calculations */
     totaltable pool_stats(get_self(), pool_id.value);
     auto total_it = pool_stats.find(pool_id.value);
     check(total_it != pool_stats.end(), "error: pool_id not found.");
-    bool locked = pool_stats->locked;
-    check(locked == true, "error: The DMD Yield Farms are inactive. Please come back later.");
+    check(pool_stats->is_active == true, "error: specified pool is inactive.");
 
-    /* Determine halving handicap */
     uint8_t mining_rate_handicap;
     uint32_t now = current_time_point().sec_since_epoch();
-
-    if (now < pool_stats->halving1_deadline)
-        mining_rate_handicap = 1;
-
-    if (now >= pool_stats->halving1_deadline)
-        mining_rate_handicap = 2;
-
-    if (now >= pool_stats->halving2_deadline)
-        mining_rate_handicap = 4;
-
-    if (now >= pool_stats->halving3_deadline)
-        mining_rate_handicap = 8;
-
-    if (now >= pool_stats->halving4_deadline)
-        mining_rate_handicap = 16;
+    /* Determine halving handicap */
+    if (now  < pool_stats->halving1_deadline)  {  mining_rate_handicap = 1;  }
+    if (now >= pool_stats->halving1_deadline)  {  mining_rate_handicap = 2;  }
+    if (now >= pool_stats->halving2_deadline)  {  mining_rate_handicap = 4;  }
+    if (now >= pool_stats->halving3_deadline)  {  mining_rate_handicap = 8;  }
+    if (now >= pool_stats->halving4_deadline)  {  mining_rate_handicap = 16; }
 
     uint32_t last_reward_time = pool_stats->last_reward_time;
-    uint16_t issue_precision  = 10000; // This means that if we set ("issue_frequency" == 100): we release 0.01 token per second.
-                                           //          and if we set ("issue_frequency" == 1):   we release 0.0001 tokens per second.
+    uint16_t issue_precision  = 10000; // With our current algorithm: if we set ("issue_frequency" == 100): we release 0.01 tokens per second.
+                                      //    and if we set ("issue_frequency" == 1):   we release 0.0001 tokens per second.
 
     /* How many coins are issued every second. Multiplied by 10000 for tokens with precision 4. Divided by "issue_precision" for extra control */
-    uint32_t dmd_issue_frequency  = pool_stats->dmd_issue_frequency*10000 / issue_precision / mining_rate_handicap;
+    uint32_t augmented_dmd_issue_frequency = pool_stats->dmd_issue_frequency*10000 / issue_precision / mining_rate_handicap;
 
     // How many seconds have passed until now and last_reward_time:
     uint32_t seconds_passed = now - last_reward_time; 
     eosio::print_f("Seconds passed since last issue(): [%] \n",seconds_passed);
 
-    // Determine how much total reward should be issued in this period for each of the coins: hub, dmd, dop.
-    uint64_t total_dmd_released = seconds_passed * dmd_issue_frequency;
+    /* Determine how much total DMD reward should be issued in this transaction/cycle/block */
+    uint64_t total_dmd_released = seconds_passed * augmented_dmd_issue_frequency;
 
-    lptable registered_accounts(get_self(), get_self().value);
+    lptable registered_accounts(get_self(), pool_id.value);
 
     /* Get total registered lptokens for this issue cycle */
     auto current_iteration = registered_accounts.begin();
@@ -130,14 +122,12 @@ void efimine::issue(uint16_t pool_id)
         /* Check the Defibox LP tables to see how many LPTokens each user has */
         asset dmd_lptokens = get_asset_amount(current_iteration->owner_account, dmd_box_lp_symbol);
 
-        uint64_t dmd_current_snapshot = dmd_lptokens.amount;
-
-        uint64_t dmd_previous_snapshot = current_iteration->dmd_snapshot_lp_amount;
-
-        uint64_t dmd_before_snapshot = current_iteration->dmd_before_lp_amount;
+        uint64_t dmd_current_snapshot  = dmd_lptokens.amount;                       /* Must definitely test this. */
+        uint64_t dmd_previous_snapshot = current_iteration->dmd_snapshot_lp_amount; /* Must definitely test this. */
+        uint64_t dmd_before_snapshot   = current_iteration->dmd_before_lp_amount;   /* Must definitely test this. */
 
         /* We'll use this to calculate the user's lp rewards and add to his unclaimed_balance */
-        uint64_t dmd_lp_calculation_amount;
+        uint64_t dmd_lp_calculation_amount; /* We'll use whatever is lower between current_snapshot and before_snapshot. */
 
         if (dmd_current_snapshot > dmd_before_snapshot)
             dmd_lp_calculation_amount = dmd_before_snapshot;
@@ -147,25 +137,23 @@ void efimine::issue(uint16_t pool_id)
         uint64_t dmd_unclaimed_amount = 0;
         /* Calculate the user's unclaimed rewards. Then we'll just += the unclaimed_amount */
 
-        if (row.dmd_staked_amount.amount > 0)
-        {
-            float dmd_percentage = float(dmd_lp_calculation_amount)/float(dmd_total_lptokens) * 100; 
-            dmd_unclaimed_amount = (dmd_percentage*total_dmd_released)/0.01/10000;} /* (divided by 10000) for coins with precision 4 */
+        if (dmd_lp_calculation_amount > 0)
+        {   float dmd_percentage = float(dmd_lp_calculation_amount)/float(dmd_total_lptokens) * 100; 
+            dmd_unclaimed_amount = (dmd_percentage*total_dmd_released)/0.01/10000;  } /* (divided by 10000) for coins with precision 4 */
 
         /* Modify the table and have before = snapshot and snapshot = current_snapshot */
         /* Add the user's unclaimed rewards if necessary */
         registered_accounts.modify(current_iteration, get_self(), [&](auto& row)
         {
-            row.dmd_before_lp_amount = dmd_previous_snapshot;
-
-            row.dmd_snapshot_lp_amount = dmd_current_snapshot;
-
-            row.dmd_unclaimed_amount += dmd_unclaimed_amount;
+            row.dmd_before_lp_amount = dmd_previous_snapshot;  /* Must definitely test this. */
+            row.dmd_snapshot_lp_amount = dmd_current_snapshot; /* Must definitely test this. */
+            row.dmd_unclaimed_amount += dmd_unclaimed_amount;  /* Must definitely test this. */
         });
 
         ++ current_iteration;
     }
     // Think about how this will work together with the NFT mechanics.
+    /* The NFTs calculation should be simple. We will check to see if user has or has not got NFT, and we will add a +10% to his farming bonus at the end. */
     // For extra points, have the lpmine contract ask for coins from another account.
 }
 
@@ -174,44 +162,39 @@ void efimine::registeruser(const name& owner_account, uint16_t pool_id)
     /* Function will check if the user has a minimum amount of LP tokens, and if not, they will not be registered for the pool. */
     require_auth(owner_account);
 
-    /* Check that the user has not already registered */
-    lptable registered_accounts(get_self(), pool_id.value); /* Need to fine-tune this syntax */
-    auto registered_it = registered_accounts.find(owner_account.value); /* Need to fine-tune this syntax */
-    check(registered_it == registered_accounts.end(), "error: User has already been registered for the pool.");
-
-    // The minimum amount for each LPToken that the user needs to have in their account to be registered. //
-    // These should be configured from the totals table //
+    /* Check pool integrity */
     totaltable pool_stats(get_self(), pool_id.value);
     auto total_it = pool_stats.find(pool_id.value);
     check(total_it != pool_stats.end(), "error: pool_id not found.");
-    bool locked = pool_stats->locked;
-    check(locked == true, "error: The DMD Yield Farms are inactive. Please come back later.");
+    check(pool_stats->is_active == true, "error: specified pool is inactive.");
 
-    minimum_dmd_amount = pool_stats->minimum_lp_tokens;
+    /* Check that the user has not already registered */
+    lptable registered_accounts(get_self(), pool_id.value); /* Need to fine-tune this syntax */
+    auto registered_it = registered_accounts.find(pool_id.value); /* Need to fine-tune this syntax */
+    check(registered_it == registered_accounts.end(), "error: User has already been registered for the pool.");
+    /* Check to see if this is the first user registering for the table with the specific pool ID or not, and either modify or emplace */
+
+    /* Probably need to change this syntax and make a loop for all the rows that fall within pool_id scope and see if we can find owner_account */
+
+    // The minimum amount for each LPToken that the user needs to have in their account to be registered. //
+    // These should be configured from the totals table //
 
     asset pool_lptokens = get_asset_amount(owner_account, pool_stats->asset_symbol);
 
     bool empty = true;
-
-    if (pool_lptokens.amount >= minimum_dmd_amount)
-    {
+    if (pool_lptokens.amount >= pool_stats->minimum_lp_tokens;)
         empty = false;
-    }
 
     check(empty == false, "User does not meet the minumum LP size requirement for the specific pool. Please add more liquidity.");
     /* Add the user in the table at this point */
     registered_accounts.emplace(get_self(), [&](auto& row)
     {
-        /* Here we'll have the `before` and the `snapshot` LP amount counter */
-        /* This is the first time they are added, so most variables are set to zero */
+        /* Here we'll have the `before` and the `snapshot` LP amount counter. */
+        /* This is the first time they are added, so most variables are set to zero. */
         row.owner_account = owner_account;
-
         row.boxlptoken_snapshot_amount = pool_lptokens.amount;
-
         row.boxlptoken_before_amount = 0;
-
         row.dmd_claimed_amount = 0;
-
         row.dmd_unclaimed_amount = 0;
     });
 }
